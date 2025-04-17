e150e143dbf9664d413c94e98c4dfd0171ee544b
Amay Tripathi <amayt@seas.upenn.edu>
2025-04-16 23:12:21 -0400

    now read files

--- betiq-react/src/pages/ActiveGames.jsx
+++ betiq-react/src/pages/ActiveGames.jsx
@@ -52,43 +52,83 @@ const scoreDataMap = {
 // NBA team logo mapping
 const teamLogos = {
     // Eastern Conference
-    "ATL": "https://cdn.nba.com/logos/nba/1610612737/global/L/logo.svg", // Hawks
-    "BOS": "https://cdn.nba.com/logos/nba/1610612738/global/L/logo.svg", // Celtics
-    "BKN": "https://cdn.nba.com/logos/nba/1610612751/global/L/logo.svg", // Nets
-    "CHA": "https://cdn.nba.com/logos/nba/1610612766/global/L/logo.svg", // Hornets
-    "CHI": "https://cdn.nba.com/logos/nba/1610612741/global/L/logo.svg", // Bulls
-    "CLE": "https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg", // Cavaliers
-    "DET": "https://cdn.nba.com/logos/nba/1610612765/global/L/logo.svg", // Pistons
-    "IND": "https://cdn.nba.com/logos/nba/1610612754/global/L/logo.svg", // Pacers
-    "MIA": "https://cdn.nba.com/logos/nba/1610612748/global/L/logo.svg", // Heat
-    "MIL": "https://cdn.nba.com/logos/nba/1610612749/global/L/logo.svg", // Bucks
-    "NYK": "https://cdn.nba.com/logos/nba/1610612752/global/L/logo.svg", // Knicks
-    "ORL": "https://cdn.nba.com/logos/nba/1610612753/global/L/logo.svg", // Magic
-    "PHI": "https://cdn.nba.com/logos/nba/1610612755/global/L/logo.svg", // 76ers
-    "TOR": "https://cdn.nba.com/logos/nba/1610612761/global/L/logo.svg", // Raptors
-    "WAS": "https://cdn.nba.com/logos/nba/1610612764/global/L/logo.svg", // Wizards
+    "Atlanta Hawks": "https://cdn.nba.com/logos/nba/1610612737/global/L/logo.svg",
+    "Boston Celtics": "https://cdn.nba.com/logos/nba/1610612738/global/L/logo.svg",
+    "Brooklyn Nets": "https://cdn.nba.com/logos/nba/1610612751/global/L/logo.svg",
+    "Charlotte Hornets": "https://cdn.nba.com/logos/nba/1610612766/global/L/logo.svg",
+    "Chicago Bulls": "https://cdn.nba.com/logos/nba/1610612741/global/L/logo.svg",
+    "Cleveland Cavaliers": "https://cdn.nba.com/logos/nba/1610612739/global/L/logo.svg",
+    "Detroit Pistons": "https://cdn.nba.com/logos/nba/1610612765/global/L/logo.svg",
+    "Indiana Pacers": "https://cdn.nba.com/logos/nba/1610612754/global/L/logo.svg",
+    "Miami Heat": "https://cdn.nba.com/logos/nba/1610612748/global/L/logo.svg",
+    "Milwaukee Bucks": "https://cdn.nba.com/logos/nba/1610612749/global/L/logo.svg",
+    "New York Knicks": "https://cdn.nba.com/logos/nba/1610612752/global/L/logo.svg",
+    "Orlando Magic": "https://cdn.nba.com/logos/nba/1610612753/global/L/logo.svg",
+    "Philadelphia 76ers": "https://cdn.nba.com/logos/nba/1610612755/global/L/logo.svg",
+    "Toronto Raptors": "https://cdn.nba.com/logos/nba/1610612761/global/L/logo.svg",
+    "Washington Wizards": "https://cdn.nba.com/logos/nba/1610612764/global/L/logo.svg",
   
     // Western Conference
-    "DAL": "https://cdn.nba.com/logos/nba/1610612742/global/L/logo.svg", // Mavericks
-    "DEN": "https://cdn.nba.com/logos/nba/1610612743/global/L/logo.svg", // Nuggets
-    "GSW": "https://cdn.nba.com/logos/nba/1610612744/global/L/logo.svg", // Warriors
-    "HOU": "https://cdn.nba.com/logos/nba/1610612745/global/L/logo.svg", // Rockets
-    "LAC": "https://cdn.nba.com/logos/nba/1610612746/global/L/logo.svg", // Clippers
-    "LAL": "https://cdn.nba.com/logos/nba/1610612747/global/L/logo.svg", // Lakers
-    "MEM": "https://cdn.nba.com/logos/nba/1610612763/global/L/logo.svg", // Grizzlies
-    "MIN": "https://cdn.nba.com/logos/nba/1610612750/global/L/logo.svg", // Timberwolves
-    "NOP": "https://cdn.nba.com/logos/nba/1610612740/global/L/logo.svg", // Pelicans
-    "OKC": "https://cdn.nba.com/logos/nba/1610612760/global/L/logo.svg", // Thunder
-    "PHX": "https://cdn.nba.com/logos/nba/1610612756/global/L/logo.svg", // Suns
-    "POR": "https://cdn.nba.com/logos/nba/1610612757/global/L/logo.svg", // Trail Blazers
-    "SAC": "https://cdn.nba.com/logos/nba/1610612758/global/L/logo.svg", // Kings
-    "SAS": "https://cdn.nba.com/logos/nba/1610612759/global/L/logo.svg", // Spurs
-    "UTA": "https://cdn.nba.com/logos/nba/1610612762/global/L/logo.svg", // Jazz
+    "Dallas Mavericks": "https://cdn.nba.com/logos/nba/1610612742/global/L/logo.svg",
+    "Denver Nuggets": "https://cdn.nba.com/logos/nba/1610612743/global/L/logo.svg",
+    "Golden State Warriors": "https://cdn.nba.com/logos/nba/1610612744/global/L/logo.svg",
+    "Houston Rockets": "https://cdn.nba.com/logos/nba/1610612745/global/L/logo.svg",
+    "Los Angeles Clippers": "https://cdn.nba.com/logos/nba/1610612746/global/L/logo.svg",
+    "Los Angeles Lakers": "https://cdn.nba.com/logos/nba/1610612747/global/L/logo.svg",
+    "Memphis Grizzlies": "https://cdn.nba.com/logos/nba/1610612763/global/L/logo.svg",
+    "Minnesota Timberwolves": "https://cdn.nba.com/logos/nba/1610612750/global/L/logo.svg",
+    "New Orleans Pelicans": "https://cdn.nba.com/logos/nba/1610612740/global/L/logo.svg",
+    "Oklahoma City Thunder": "https://cdn.nba.com/logos/nba/1610612760/global/L/logo.svg",
+    "Phoenix Suns": "https://cdn.nba.com/logos/nba/1610612756/global/L/logo.svg",
+    "Portland Trail Blazers": "https://cdn.nba.com/logos/nba/1610612757/global/L/logo.svg",
+    "Sacramento Kings": "https://cdn.nba.com/logos/nba/1610612758/global/L/logo.svg",
+    "San Antonio Spurs": "https://cdn.nba.com/logos/nba/1610612759/global/L/logo.svg",
+    "Utah Jazz": "https://cdn.nba.com/logos/nba/1610612762/global/L/logo.svg"
+};
+
+// Get team name from team name (e.g., "BKN Nets" -> "Brooklyn Nets")
+const getTeamCode = (teamName) => {
+  // If the team name is already in the correct format, return it
+  if (teamLogos[teamName]) {
+    return teamName;
+  }
+  
+  // Map of team codes to full team names
+  const teamCodeToName = {
+    "ATL": "Atlanta Hawks",
+    "BOS": "Boston Celtics",
+    "BKN": "Brooklyn Nets",
+    "CHA": "Charlotte Hornets",
+    "CHI": "Chicago Bulls",
+    "CLE": "Cleveland Cavaliers",
+    "DET": "Detroit Pistons",
+    "IND": "Indiana Pacers",
+    "MIA": "Miami Heat",
+    "MIL": "Milwaukee Bucks",
+    "NYK": "New York Knicks",
+    "ORL": "Orlando Magic",
+    "PHI": "Philadelphia 76ers",
+    "TOR": "Toronto Raptors",
+    "WAS": "Washington Wizards",
+    "DAL": "Dallas Mavericks",
+    "DEN": "Denver Nuggets",
+    "GSW": "Golden State Warriors",
+    "HOU": "Houston Rockets",
+    "LAC": "Los Angeles Clippers",
+    "LAL": "Los Angeles Lakers",
+    "MEM": "Memphis Grizzlies",
+    "MIN": "Minnesota Timberwolves",
+    "NOP": "New Orleans Pelicans",
+    "OKC": "Oklahoma City Thunder",
+    "PHX": "Phoenix Suns",
+    "POR": "Portland Trail Blazers",
+    "SAC": "Sacramento Kings",
+    "SAS": "San Antonio Spurs",
+    "UTA": "Utah Jazz"
   };
 
-// Get team code from team name (e.g., "BKN Nets" -> "BKN")
-const getTeamCode = (teamName) => {
-  return teamName.split(" ")[0];
+  const teamCode = teamName.split(" ")[0];
+  return teamCodeToName[teamCode] || teamName;
 };
 
 // Generate the last 7 days for date selector
@@ -124,10 +164,6 @@ const SPORTSBOOKS = {
   FANDUEL: {
     name: "FanDuel",
     logo: "https://logo.clearbit.com/fanduel.com",
-  },
-  ESPNBET: {
-    name: "ESPN BET",
-    logo: "https://logo.clearbit.com/espn.com",
   }
 };
 
@@ -275,7 +311,6 @@ const ActiveGames = () => {
         const keyMapping = {
           DRAFTKINGS: 'draftkings',
           FANDUEL: 'fanduel',
-          ESPNBET: 'espnBet'
         };
         
         return (
